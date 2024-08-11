9f61221ca69a27e0fa23ef6831165538b9f8df91
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-08-11 22:25:27 +0800

    Added Ship class

--- Ship.mjs
+++ Ship.mjs
@@ -1,7 +1,26 @@
-class Ship {
-    constructor(length) {
-        this.length = length;
+export class Ship {
+    constructor() {
+        this.length = 0;
         this.hits = 0;
-        this.sunk = false;
+    }
+
+    setLength(length) {
+        this.length = length;
+    }
+
+    getLength() {
+        return this.length;
+    }
+
+    getHit() {
+        return this.hits;
+    }
+
+    hit() {
+        this.hits++;
+    }
+
+    isSunk() {
+        return this.hits >= this.length ? true : false;
     }
 }
--- Ship.test.js
+++ Ship.test.js
@@ -1,7 +1,23 @@
-test("reverse string", () => {
-    expect(hello()).toBe(3);
+import { test, expect } from "@jest/globals";
+import { Ship } from "./Ship.mjs";
+
+test("Hit test", () => {
+    const ship = new Ship();
+    ship.setLength(3);
+
+    expect(ship.getHit()).toBe(0);
+    ship.hit();
+    expect(ship.getHit()).toBe(1);
 });
 
-function hello() {
-    return 3;
-}
+test("Sink test", () => {
+    const ship1 = new Ship();
+    ship1.setLength(1);
+    ship1.hit();
+    expect(ship1.isSunk()).toBe(true);
+
+    const ship2 = new Ship();
+    ship2.setLength(3);
+    ship2.hit();
+    expect(ship2.isSunk()).toBe(false);
+});
